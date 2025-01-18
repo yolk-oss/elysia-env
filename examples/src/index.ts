16e1d1b8684abcc8cb2565a5e90d12595ce1db5a
Fecony <minepuff@gmail.com>
2025-01-18 17:50:33 +0200

    fix: update dependencies and fix schema validation bug

---
+++ examples/src/index.ts
@@ -0,0 +1,22 @@
+import { Elysia, t } from 'elysia'
+import { env } from '@yolk-oss/elysia-env'
+
+const app = new Elysia({
+    prefix: '/good',
+})
+    .use(
+        env({
+            TOKEN: t.String({
+                minLength: 5,
+                error: 'TOKEN is required for service A!',
+            }),
+            IS_ENABLED: t.Boolean({ default: false }),
+            PORT: t.Number({
+                default: 3000,
+            }),
+        }),
+    )
+    .get('/', ({ env }) => env)
+    .listen(4242, ({ hostname, port }) => {
+        console.log(`🦊 📂 - Listening on http://${hostname}:${port}`)
+    })
--- src/index.ts
+++ src/index.ts
@@ -9,37 +9,31 @@ export const env = <TEnv extends TProperties = NonNullable<unknown>>(
     new Elysia({
         name: '@yolk-oss/elysia-env',
     }).decorate(() => {
-        const envVariableSchema = t.Object(variables)
         // TODO: allow to pass env to take data from?
         const runtimeEnv = process.env
-        const typeCompiler = TypeCompiler.Compile(envVariableSchema)
 
-        const preparedVariables = Value.Clean(
-            envVariableSchema,
-            Value.Convert(
-                envVariableSchema,
-                Value.Default(envVariableSchema, runtimeEnv),
-            ),
+        const EnvVariableSchema = t.Object(variables)
+        const Compiler = TypeCompiler.Compile(EnvVariableSchema)
+
+        const preparedVariables = Value.Parse(
+            ['Clone', 'Clean', 'Default', 'Decode', 'Convert'],
+            EnvVariableSchema,
+            runtimeEnv,
         )
 
-        if (!typeCompiler.Check(preparedVariables)) {
+        if (!Compiler.Check(preparedVariables)) {
             console.error(
                 '❌ Invalid environment variables:',
-                [...typeCompiler.Errors(preparedVariables)].reduce(
-                    (errors, e) => {
-                        const path = e.path.substring(1)
-                        return { ...errors, [path]: e.message }
-                    },
-                    {},
-                ),
+                [...Compiler.Errors(preparedVariables)].reduce((errors, e) => {
+                    const path = e.path.substring(1)
+                    return { ...errors, [path]: e.message }
+                }, {}),
             )
 
-            // TODO: ask user if they want it to stop the process or just show error in console
             process.exit()
         }
 
-        // TODO: add success/error callback as params?
         return {
-            env: preparedVariables as Static<typeof envVariableSchema>,
+            env: preparedVariables as Static<typeof EnvVariableSchema>,
         }
     })
